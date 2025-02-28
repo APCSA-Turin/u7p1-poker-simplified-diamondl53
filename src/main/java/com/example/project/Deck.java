41836229daddffa415291ca54f422fb8a4d90543
diamondl53 <diamondl53@nycstudents.net>
2025-02-28 13:18:26 +0000

    second commit

--- src/main/java/com/example/project/Deck.java
+++ src/main/java/com/example/project/Deck.java
@@ -16,7 +16,11 @@ public class Deck{
     }
 
     public  void initializeDeck(){ //hint.. use the utility class
-        
+        for(int i = 0; i < Utility.getRanks().length; i++){
+            for(int k = 0; k < Utility.getSuits().length; k++){
+                cards.add(New Car ())
+            }
+        }
     }
 
     public  void shuffleDeck(){ //You can use the Collections library or another method. You do not have to create your own shuffle algorithm
